781dad5239c2f7a2d9c029109f09fdf7a175e0c9
Atulanand09 <atulanand092004@gmail.com>
2025-10-27 20:15:39 +0530

    Added Chai

---
+++ 00_python/non_python_shop.py
@@ -0,0 +1,34 @@
+# Just exprience it not a code file.
+# So, please ignore this file.
+# --- IGNORE ---
+
+class chai:
+    def __init__(self, temperature, sweetness, strength, milk_level):
+        self.tempurature = temperature
+        self.sweetness = sweetness
+        self.strength = strength
+        self.milk_level = milk_level
+        
+    def sip(self):
+        print("Sipping the chai")
+        
+    def add_milk(self, amount):
+        self.milk_level += amount
+        print(f"Added {amount} ml of milk to the chai")
+        
+    def add_sugar(self, amount):
+        self.sweetness += amount
+        print(f"Added {amount} grams of sugar to the chai")
+        
+    def temperature_check(self):
+        print(f"The chai tempurature is {self.tempurature} degrees Celsius")
+        
+    def strength_check(self):
+        print(f"The chai strength is {self.strength} units")
+        
+chai = chai(temperature=60, sweetness=5, strength=7, milk_level=100)
+chai.sip()
+chai.add_milk(50)
+chai.add_sugar(10)
+chai.temperature_check()
+chai.strength_check()
