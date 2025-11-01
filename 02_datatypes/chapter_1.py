8afb95036dacae87dbc83ba7d247732cfa2caf64
Atulanand09 <atulanand092004@gmail.com>
2025-11-01 15:07:42 +0530

    datatypes

---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,8 @@
+sugar_amount = 10
+print(f"Initial sugar: {sugar_amount}") 
+
+sugar_amount = 15
+print(f"Second Initial sugar: {sugar_amount}") 
+
+print(f"ID of 10: {id(10)}")
+print(f"ID of 15: {id(15)}")
